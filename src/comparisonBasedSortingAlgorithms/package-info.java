987d857559ed602f43cc93b9206879d1823aa170
Rawad Alaryan <rawadalaryan@hotmail.com>
2020-06-11 15:53:08 -0400

    Bogo Sorting Algorithm In Java

---
+++ src/comparisonBasedSortingAlgorithms/package-info.java
@@ -0,0 +1,14 @@
+/**
+* Comparison-Based Sorting Algorithms In Java.
+* <p>
+* This package contains: 
+* Bogo Sorting Algorithm.
+* 
+* </p>
+*
+* @since 1.0
+* @author Rawad Alaryan
+* @version 1.0
+*/
+
+package comparisonBasedSortingAlgorithms;
