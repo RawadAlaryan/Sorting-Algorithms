8ba5a862e8480da43e1aedb71318f45120eaa6e6
Rawad Alaryan <rawadalaryan@hotmail.com>
2020-06-13 20:34:37 -0400

    Minor Changes.

--- src/main/java/nonComparisonBasedSortingAlgorithms/package-info.java
+++ src/main/java/nonComparisonBasedSortingAlgorithms/package-info.java
@@ -1,14 +1,16 @@
 /**
-* Non-Comparison-Based Sorting Algorithms In Java.
-* <p>
-* This package contains: 
-* 
-* 
-* </p>
-*
-* @since 1.0
-* @author Rawad Alaryan
-* @version 1.0
+Non-Comparison-Based Sorting Algorithms In Java.
+<p>This package contains:</p>
+	Counting Sorting Algorithm.
+	Pigeon-Hole Sorting Algorithm.
+	Radix Sorting Algorithm.
+	Bucket Sorting Algorithm.
+	AmericanFlag Sorting Algorithm.
+	Bead Sorting Algorithm.
+
+@since 1.0
+@author Rawad Alaryan
+@version 1.0
 */
 
 package main.java.nonComparisonBasedSortingAlgorithms;
